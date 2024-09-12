05ad3ac4201705734c5a8b512a08594742af85fa
Abhinav7982 <abhinavmuz7982@gmail.com>
2024-09-12 22:17:42 +0530

    updated

--- models/migration/silver_table.sql
+++ models/migration/silver_table.sql
@@ -7,6 +7,7 @@ silver_table AS (
         Invoice, 
         StockCode,  
         Quantity, 
+        Description,
         InvoiceDate, 
         Price,
         (Quantity * Price) AS TotalValue,  -- Calculate total value for the line item
